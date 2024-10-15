94f2420e11e39979a351631533c7518e99631d46
Lucascara23 <scarabattoliluca23@gmail.com>
2024-10-15 08:57:31 +0200

    primo commit

---
+++ src/main/java/com/mycompany/progetto/Progetto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progetto;
+
+/**
+ *
+ * @author lucascarabattoli
+ */
+public class Progetto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
